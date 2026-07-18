c801decbbd3f665125c0db4f131fcd3efb0daf7a
Jeremy Aldama <jeremyaldama23.2@gmail.com>
2026-07-18 10:45:36 -0500

    chore(version): bump versionCode to 9 and versionName to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.qolve.fluyo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
